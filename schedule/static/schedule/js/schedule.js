5886d73ea3e0ab68caaa6f147370a832e7ed6d55
Drykath Dragon <drykath@motorcityfurrycon.org>
2021-08-31 00:47:15 +0000

    Schedule UI improvement: Support popovers better
    Not sure if this is an old bug or new browser behavior, but clicking on
    a schedule item was showing the popover version but still jumping right
    to its detail page. This should prevent that, still allowing the detail
    link at the bottom of the popover to link to that page.
    
    That said, some of the code around that is fairly inscrutable, so
    there's definitely some room for improvement there anyway.

--- schedule/static/schedule/js/schedule.js
+++ schedule/static/schedule/js/schedule.js
@@ -22,6 +22,9 @@ $(function () {
   }
   $('body').on('click', function(e) {
     $('[data-toggle="popover"],[data-original-title]').each(function() {
+      if ($(this).is(e.target)) {
+        e.preventDefault();
+      }
       if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
         (($(this).popover('hide').data('bs.popover')||{}).inState||{}).click = false;
       }
