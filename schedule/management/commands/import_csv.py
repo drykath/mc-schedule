f38fcbf6148ec722854fca7024a26b2359857771
Drykath Dragon <drykath@motorcityfurrycon.org>
2023-02-28 01:33:23 +0000

    Programming CSV import tweaks for 2023

--- schedule/management/commands/import_csv.py
+++ schedule/management/commands/import_csv.py
@@ -40,8 +40,6 @@ class Command(BaseCommand):
         if len(existing) > 0:
             if options['clear']:
                 existing.delete()
-                for room in Room.objects.filter(convention=convention):
-                    room.schedule.clear()
             else:
                 if not options['append']:
                     raise CommandError('Convention already has panels, cannot import without --append or --clear')
@@ -51,63 +49,85 @@ class Command(BaseCommand):
         room_cache = {room.name: room for room in rooms}
         # If the room is called a different thing, or has a specific purpose
         room_cache.update({room.alias: room for room in rooms if room.alias})
+        # Upper case versions to match CSV export
+        room_cache.update({room.name.upper(): room for room in rooms})
+        room_cache.update({room.alias.upper(): room for room in rooms if room.alias})
 
         tracks = Track.objects.filter(convention=convention)
         track_cache = {track.name: track for track in tracks}
 
         # Maps to correct shortcuts in the source data
         time_map = {
-            'Always Open': ['THU: 1800-0030', 'FRI: 0900-0200', 'SAT: 0900-0200', 'SUN: 0900-0200'],
+            'ALWAYS OPEN': ['THU: 1800-0030', 'FRI: 0900-0200', 'SAT: 0900-0200', 'SUN: 0900-0200'],
         }
         time_regex = re.compile('^(?P<day>\w{3}): (?P<shour>\d{2})(?P<sminute>\d{2})-(?P<ehour>\d{2})(?P<eminute>\d{2})\s*$')
         time_days = {
-            'THU': 3,
-            'FRI': 4,
-            'SAT': 5,
-            'SUN': 6,
+            'THURSDAY': 3,
+            'FRIDAY': 4,
+            'SATURDAY': 5,
+            'SUNDAY': 6,
         }
 
+        line = 1
         with open(options['csv_path'], 'r') as schedulefile:
             for row in csv.DictReader(schedulefile):
-                # Ignore items not really on the schedule
-                if row['Room / Time'] == 'Unscheduled' or row['Room / Time'].startswith('CANCELLED'):
+                line += 1
+                # Ignore room times, this year these seem complex enough that it's easier to enter manually than parse
+                if row['Panel Name'] in (
+                            "",
+                            "Panel Name",
+                            "Fursuit Headless Lounge",
+                            "Artist Quiet Area",
+                            "Artist's Alley Sign Up",
+                            "Registration - Pre-reg Only",
+                            "Registration",
+                            "Video Game Room",
+                            "Con Store",
+                            "Con Store - Pre-Reg Swag Pick Up",
+                            "Tabletop Gaming",
+                            "Artist's Alley",
+                            "Dealer's Den",
+                            "Sponsor's Lounge",
+                            "Fursuit Dance Prelims", # Remember to add that one in manually, too
+                            "Vintage Fursuits", # Remember to add that one in manually, too
+                        ) or row['Panel Name'].startswith('Fuzzy Logic'):
                     continue
                 if 'Exclude' in row and row['Exclude'] == 'Yes':
                     continue
 
                 # Map rooms and times
-                room_label = row['Room / Time'].split('\n')[-1]
+                room_label = row['ROOM']
+                track_label = row['Track']
+                if track_label == '':
+                    track_label = 'Other'
 
+                if not room_label in room_cache:
+                    raise Exception('Room "{}" unknown on line {}'.format(room_label, line))
+                if not track_label in track_cache:
+                    raise Exception('Track "{}" unknown on line {}'.format(track_label, line))
+
+                if debug:
+                    print(row)
                 if row['Panel Name'] in room_cache.keys():
                     item = room_cache[row['Panel Name']]
                 else:
                     item = Panel.objects.create(
                         convention = convention,
                         title = row['Panel Name'],
-                        track = track_cache[row['Track']],
+                        track = track_cache[track_label],
                         room = room_cache[room_label],
                         hosts = row['Hosts'],
                         description = row['Conbook Description'],
                     )
 
-                times = row['Room / Time'].split('\n')[0:-1]
-                if len(times) == 0:
-                    raise Exception('No time information in row {}'.format(row))
-                if times[0] in time_map:
-                    times = time_map[times[0]]
-                for panel_time in times:
-                    time_info = time_regex.search(panel_time)
-                    if not time_info:
-                        raise Exception('Bad time information in row {}'.format(row))
-                    time_info = time_info.groupdict()
-                    # Special case, make 24:00 into 0:00 midnight
-                    if time_info['shour'] == '24':
-                        time_info['shour'] = '0'
-                    if time_info['ehour'] == '24':
-                        time_info['ehour'] = '0'
-                    # Add schedule to either panel or room times
-                    item.schedule.create(
-                        day = time_days[time_info['day']],
-                        start_time = time(int(time_info['shour']), int(time_info['sminute'])),
-                        end_time = time(int(time_info['ehour']), int(time_info['eminute'])),
-                    )
+                #if times[0] in time_map:
+                #    times = time_map[times[0]]
+                #for panel_time in times:
+                start_time = datetime.strptime(row['Start Time'], "%I:%M %p").time()
+                end_time = datetime.strptime(row['End Time'], "%I:%M %p").time()
+                # Add schedule to either panel or room times
+                item.schedule.create(
+                    day = time_days[row['Day'].upper()],
+                    start_time = start_time,
+                    end_time = end_time,
+                )
